94ede6352a904457f5a87467e34476bf432f1463
DashaPoliazz <dasha.poliazzz@gmail.com>
2023-07-26 22:12:25 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Box, Typography, Stack, Button } from "@mui/material";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleIncrement = () => setCounter(counter + 1);
+  const handleDecrement = () => setCounter(counter - 1);
+  const handleAsyncIncrement = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setCounter(counter + 1);
+      setIsLoading(false);
+    }, 2000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        backgroundColor: "lightcoral",
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Stack spacing={4} alignItems="center">
+        <Typography fontWeight={900} fontSize={22}>
+          Counter:
+        </Typography>
+        <Typography fontSize={70}>{counter}</Typography>
+        <Stack direction="row" spacing={2}>
+          <Button
+            onClick={handleIncrement}
+            disabled={isLoading}
+            variant="contained"
+            color="success"
+          >
+            Plus
+          </Button>
+          <Button
+            onClick={handleDecrement}
+            disabled={isLoading}
+            variant="contained"
+            color="error"
+          >
+            Minus
+          </Button>
+          <Button
+            onClick={handleAsyncIncrement}
+            disabled={isLoading}
+            variant="contained"
+            color="primary"
+          >
+            Add async
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
   );
 }
 
